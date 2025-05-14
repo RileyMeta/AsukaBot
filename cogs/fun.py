48e1c8d80cbade5c7e68969fa609f07565ff6963
Cory (Riley) Black <32332593+RileyMeta@users.noreply.github.com>
2025-05-14 17:48:37 -0400

    Update fun.py
    + Added choice command
    + aDdEd SpOnGeBoB mOcKiNg
    + Added UwU command

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import random
+import re
 
 class Fun(commands.Cog):
     def __init__(self, bot):
@@ -48,6 +49,45 @@ class Fun(commands.Cog):
     async def say(self, ctx, *, message: str = "Idiot forgot to make me say something"):
         await ctx.message.delete()
         await ctx.send(message)
+
+    @commands.command(name="choose", help="Have the bot randomly choose from a list of options")
+    @commands.has_permissions(send_messages=True)
+    async def choose(self, ctx, *options):
+        if len(options) < 2:
+            await ctx.send("Baka, provide at least 2 options!")
+            return
+
+        choice = random.randrange(len(options) - 1)
+        await ctx.send(f"I choose {options[choice]}")
+
+    @commands.command(name="mock", help="hAvE ThE bOt MoCk A MeSsAgE")
+    @commands.has_permissions(send_messages=True)
+    async def mock(self, ctx, *, text):
+        mocked = ""
+        upper = False
+        for char in text:
+            if char.isalpha():
+                mocked += char.upper() if upper else char.lower()
+                upper = not upper
+            else:
+                mocked += char
+
+        await ctx.send(mocked)
+
+    @commands.command(name="uwu", help="UwU-ify any message")
+    @commands.has_permissions(send_messages=True)
+    async def uwu(self, ctx, *, text):
+        faces = ["(・`ω´・)", "uwu", ">w<", "^w^", "(*^ω^)", "(⑅˘꒳˘)", "(✿◠‿◠)"]
+
+        uwu_text = text
+        uwu_text = re.sub(r'[rl]', 'W', uwu_text)
+        uwu_text = re.sub(r'[RL]', 'W', uwu_text)
+        uwu_text = re.sub(r'n([aeiou])', r'ny\1', uwu_text)
+        uwu_text = re.sub(r'N([aeiouAEIOU])', r'Ny\1', uwu_text)
+
+        uwu_text += " " + random.choice(faces)
+
+        await ctx.send(uwu_text)
 
 def setup(bot):
     bot.add_cog(Fun(bot))
